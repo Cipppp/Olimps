70ac57be3df27aef4413a632dd385d33ccae565a
Cipppp <pirvuciprian37@gmail.com>
2023-03-26 19:16:07 +0300

    Added 10 problems to practice

---
+++ Practice/basic.py
@@ -0,0 +1,10 @@
+# 1. Write a Python program that asks the user to enter their name and greets them by name.
+# 2. Write a Python program that takes in a number and prints whether it's odd or even.
+# 3. Write a Python program that takes in a list of numbers and prints the sum of all the numbers in the list.
+# 4. Write a Python program that takes in a list of words and prints the longest word in the list.
+# 5. Write a Python program that takes in a string and prints the string in reverse order.
+# 6. Write a Python program that takes in two numbers and prints their product.
+# 7. Write a Python program that takes in a list of numbers and prints the average of all the numbers in the list.
+# 8. Write a Python program that takes in a list of words and prints the number of words in the list that start with a vowel.
+# 9. Write a Python program that takes in a number and prints the factorial of that number.
+# 10. Write a Python program that takes in a string and replaces all the vowels with the letter "x".
